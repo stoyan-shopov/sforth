8d7c753c492ec184bea80e33cdbb082d8022e628
Stoyan Shopov <stoyan.shopov@gmail.com>
2016-04-11 19:52:47 +0300

    amending commit for the 'hdump' word
    this casts out C headers not needed if the 'hdump'
    word is not used

--- sf-opt-file.c
+++ sf-opt-file.c
@@ -19,10 +19,13 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+
+#if ENABLE_HDUMP
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#endif /* ENABLE_HDUMP */
 
 #include "engine.h"
 
